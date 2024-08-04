7756a47b597d32b8c9fd0c7238c244669147f6f6
unknown <ashirbad200nayak@gmail.com>
2024-08-05 01:47:45 +0530

    Portfolio 1st commit

---
+++ src/Components/portfolio/Portfolio.jsx
@@ -0,0 +1,85 @@
+import { useRef } from "react";
+import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    title: "React Currency Converter",
+    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: "In this project, i have build a very simple currency converter app with the help of an API. my app contains three sections, one for taking the user input and storing it inside a state variable, a menu where users can change the units of conversion, and finally, a display section where we display the final results.",
+  },
+  {
+    id: 2,
+    title: "Mern Blog Page",
+    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: "This project is a blog website developed using the MERN (MongoDB, Express.js, React, Node.js) stack. The project includes basic blog functionalities such as user registration, login, post management, commenting, and liking.",
+  },
+  {
+    id: 3,
+    title: "Linkedin Automation bot",
+    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: "LinkedIn automation software is specifically designed to automate an array of repetitive tasks for LinkedIn marketing. These tools integrate with LinkedIn using APIs provided by the platform itself to streamline your tasks. Some of these tools are diverse in their functionalities and some address very specific needs for users.",
+  },
+  {
+    id: 4,
+    title: "Music App",
+    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "The Full Stack MERN Music App is a modern web application that allows users to discover, listen to, and manage their favorite songs. Built using the MERN (MongoDB, Express.js, React.js, Node.js) stack, this application offers a seamless and immersive music experience.",
+  },
+];
+
+const Single = ({ item }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section >
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{y}}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Demo</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Portfolio = () => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Portfolio;
